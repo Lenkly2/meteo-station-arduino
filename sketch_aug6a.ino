e618bbb1c807a6d00695bad08d7846bf59a831bb
Lenkly2 <143933790+Lenkly2@users.noreply.github.com>
2025-08-28 15:59:19 +0300

    Add files via upload

--- sketch_aug6a.ino
+++ sketch_aug6a.ino
@@ -3,15 +3,22 @@
 #include <Wire.h>
 LiquidCrystal_I2C lcd(0x27,16,2);
 GyverBME280 bme;
-bool ab;
+bool click;
+bool click2;
 bool backlight = 0;
 int arraynumber = 0;
 int count = 0;
+int screen = 0;
+float Humidity;
+float Temperature;
+float Pressure;
 float TempArrayH[6] = {};
 float HumArrayH[6] = {};
 float PressArrayH[6] = {};
+float TempArrayD[] = {};
+float HumArrayD[] = {};
+float PressArrayD[] = {};
 
-int aoo = 0;
 void setup() {
   Serial.begin(9600);
   lcd.init();
@@ -19,18 +26,19 @@ void setup() {
   bme.setStandbyTime(STANDBY_1000MS);
   bme.begin();
   pinMode(7,INPUT);
+  pinMode(4,INPUT);
   
 }
 
-int button(){
-  ab = digitalRead(7);
-  if (ab == 1){
+int ButtonRight(){
+  click2 = digitalRead(4);
+  if (click2 == 1){
     for (int a = 0;a < 8;a++){
       delay(100);
     }
-    ab = digitalRead(7);
+    click2 = digitalRead(4);
 
-    if (ab == 1){
+    if (click2 == 1){
       if (backlight == 1){
         lcd.noBacklight();
       }else{
@@ -40,23 +48,38 @@ int button(){
         backlight = 0;
       }else{
         backlight = 1;
-
       }
-      return 1;
+
     }else{
-      return 2;
+      screen = 0;
+      SensorRead();
     }
   }else{
     return 0;
   }
+}
 
+int Button(){
+  click = digitalRead(7);
+  if (click == 1){
+    
+    delay(100);
+    
+    screen +=1;
+    if (screen > 6){
+      screen = 0;
+    }
+    
+  }else{
+    return 0;
+  }
 }
 
 void SensorRead(){
 
-  float Humidity = bme.readHumidity();
-  float Temperature = bme.readTemperature();
-  float Pressure = bme.readPressure(); 
+  Humidity = bme.readHumidity();
+  Temperature = bme.readTemperature();
+  Pressure = bme.readPressure(); 
 
   lcd.setCursor(0, 0);
   lcd.print(Temperature);
@@ -73,9 +96,9 @@ void SensorRead(){
 }
 
 int DataHour(int num){
-  float Humidity = bme.readHumidity();
-  float Temperature = bme.readTemperature();
-  float Pressure = bme.readPressure(); 
+  Humidity = bme.readHumidity();
+  Temperature = bme.readTemperature();
+  Pressure = bme.readPressure(); 
   Serial.println(pressureToMmHg(Pressure));
   TempArrayH[num] = Temperature;
   HumArrayH[num] = Humidity;
@@ -83,33 +106,56 @@ int DataHour(int num){
 }
 
 void loop() {
+  if (screen == 0){
+    SensorRead();
+  }
   
-  SensorRead();
   count +=1;
 
-  if (count % 60 == 0){
+  if (count % 10 == 0){
     DataHour(arraynumber);
     arraynumber +=1;
+    if (count % 60 == 0){
+      count = 0;
+      arraynumber = 0;
+    }
   }
   for (int i = 0; i < 60; i++){
 
     delay(1000);
-    int but = button();
-    lcd.setCursor(8,1);
-    lcd.print(i);
-
-    // if (skrentime < 5){
-    //   skrentime +=1;
-    // }else{
-    //   lcd.noBacklight();
-    // }
-
-    if (but == 1){
-      // skrentime = 0;
-      // lcd.backlight();
-      SensorRead();
-      delay(1);
+    Button();
+    ButtonRight();
+    switch(screen){
+      case 0:
+        lcd.setCursor(8,1);
+        lcd.print(i);
+        break;
+      case 1:
+        lcd.clear();
+        lcd.print("TempH");
+        break;
+      case 2:
+        lcd.clear();
+        lcd.print("HumH");
+        break;
+      case 3:
+        lcd.clear();
+        lcd.print("PressH");
+        break;
+      case 4:
+        lcd.clear();
+        lcd.print("TempD");
+        break;
+      case 5:
+        lcd.clear();
+        lcd.print("HumD");
+        break;
+      case 6:
+        lcd.clear();
+        lcd.print("PressD");
+        break;
     }
+    
   }
   
   
